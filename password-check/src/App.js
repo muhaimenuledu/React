2e6b8b3baeccd7f3eab057ebe2e73de80f6fd98c
Muhaimenul Islam <90051115+muhaimenuledu@users.noreply.github.com>
2023-01-26 15:55:12 +0600

    Add files via upload

---
+++ password-check/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import PasswordAndConfirmPasswordValidation from "./PasswordAndConfirmPasswordValidation";
+
+function App() {
+  return (
+    <>
+      <PasswordAndConfirmPasswordValidation />
+    </>
+  );
+}
+
+export default App;
